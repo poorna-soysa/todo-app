c0ea11b6f41348b912870e4328d554de0127df34
Poorna Soysa <poornapiumal.soysa@gmail.com>
2024-04-12 16:55:41 +0530

    Moved todo endpoints to entension emthod

---
+++ src/TodoApp.API/EndpointExtensions.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApp.API.Data;
+using TodoApp.API.Dtos;
+using TodoApp.API.Model;
+
+namespace TodoApp.API;
+
+public static class EndpointExtensions
+{
+    private const string TodoEndpoint = "/todos";
+    public static void AddTodoEndpoints(this IEndpointRouteBuilder app)
+    {
+        app.MapGet(TodoEndpoint, async (TodoDb db) =>
+       await db.TodoItems.ToListAsync());
+
+        app.MapGet($"{TodoEndpoint}/{{id}}", async (int id, TodoDb db) =>
+               await db.TodoItems.FindAsync(id));
+
+        app.MapGet("/todos/complete", async (TodoDb db) =>
+               await db.TodoItems.Where(d => d.IsCompleted).ToListAsync());
+
+        app.MapPost(TodoEndpoint, async (CreateTodoItemDto request, TodoDb db) =>
+        {
+            TodoItem todo = new() { Name = request.Name };
+
+            db.TodoItems.Add(todo);
+            await db.SaveChangesAsync();
+
+            return Results.Created($"{TodoEndpoint}/{todo.Id}", todo);
+        });
+
+        app.MapPut($"{TodoEndpoint}/{{id}}", async (int id, UpdateTodoItemDto request, TodoDb db) =>
+        {
+            var todo = await db.TodoItems.FindAsync(id);
+
+            if (todo is null)
+            {
+                return Results.NotFound();
+            }
+
+            todo.Name = request.Name;
+            todo.IsCompleted = request.IsCompleted;
+            await db.SaveChangesAsync();
+
+            return Results.NoContent();
+        });
+
+        app.MapDelete($"{TodoEndpoint}/{{id}}", async (int id, TodoDb db) =>
+        {
+            var todo = await db.TodoItems.FindAsync(id);
+
+            if (todo is null)
+            {
+                return Results.NotFound();
+            }
+
+            db.TodoItems.Remove(todo);
+            await db.SaveChangesAsync();
+
+            return Results.NoContent();
+        });
+    }
+}
--- src/TodoApp.API/Program.cs
+++ src/TodoApp.API/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using TodoApp.API;
 using TodoApp.API.Data;
 using TodoApp.API.Dtos;
 using TodoApp.API.Model;
@@ -9,54 +10,7 @@ builder.Services.AddDbContext<TodoDb>(options => options.UseInMemoryDatabase("To
 
 var app = builder.Build();
 
-app.MapGet("/todos", async (TodoDb db) =>
-       await db.TodoItems.ToListAsync());
-
-app.MapGet("/todos/{id}", async (int id, TodoDb db) =>
-       await db.TodoItems.FindAsync(id));
-
-app.MapGet("/todos/complete", async (TodoDb db) =>
-       await db.TodoItems.Where(d=> d.IsCompleted).ToListAsync());
-
-app.MapPost("/todos", async (CreateTodoItemDto request, TodoDb db) =>
-{
-    TodoItem todo = new() { Name = request.Name };
-
-    db.TodoItems.Add(todo);
-    await db.SaveChangesAsync();
-
-    return Results.Created($"todos/{todo.Id}", todo);
-});
-
-app.MapPut("/todos/{id}", async (int id, UpdateTodoItemDto request, TodoDb db) =>
-{
-    var todo = await db.TodoItems.FindAsync(id);
-
-    if (todo is null)
-    {
-        return Results.NotFound();
-    }
-
-    todo.Name = request.Name;
-    todo.IsCompleted = request.IsCompleted;
-    await db.SaveChangesAsync();
-
-    return Results.NoContent();
-});
-
-app.MapDelete("/todos/{id}", async (int id, TodoDb db) =>
-{
-    var todo = await db.TodoItems.FindAsync(id);
-
-    if (todo is null)
-    {
-        return Results.NotFound();
-    }
-
-    db.TodoItems.Remove(todo);
-    await db.SaveChangesAsync();
-
-    return Results.NoContent();
-});
+// Add todo endpoints 
+app.AddTodoEndpoints();
 
 app.Run();
